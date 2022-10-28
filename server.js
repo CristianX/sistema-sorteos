295a28dacf91b27f2f81eecf5fdf6669c0355dfb
Cristian Xavier Tapia Vargas <thecristianx@hotmail.com>
2022-10-28 09:25:43 -0500

    cambios para subir a heroku v3

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/angular-app-heroku'));
 
-app.get('/*', (req, res) =>{}
-    //res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
 );
 
 // Start the app by listening on the default Heroku port
